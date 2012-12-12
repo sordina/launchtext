078b83784114e5abc6144237734a8d16d6372bbe
Lyndon Maydwell <lyndon@sordina.net>
2012-12-12 13:47:46 +0800

    Working blit function

--- src/launchtext/web.clj
+++ src/launchtext/web.clj
@@ -4,14 +4,17 @@
 (use 'compojure.route)
 (use 'compojure.handler)
 (use 'ring.adapter.jetty)
+(use 'ring.middleware.params)
+(use 'launchtext.blit)
 
 (def html "<!DOCTYPE HTML>\n <html> <head> <meta http-equiv='content-type' content='text/html; charset=utf-8'> <title>Launcpad Message</title> </head> <body> <h1>Launchpad Message</h1> <form action='/' method='post'> <p><input type='text' name='message'></p> <p><input type='submit' value='Post!'></p> </form> </body> </html>")
 
 (defroutes
   main-routes (GET       "/" [] html)
-              (POST      "/" [] (do html))
-              (not-found        "<h1>Page not found</h1>"))
+              (POST      "/" [message] (do (prn (blit message)) html))
+              (not-found "<h1>Page not found</h1>"))
 
-(defn app [] (-> main-routes))
 
-(defn start-web [] (run-jetty (app) {:port 9876  :join? false}))
+(def app (-> #'main-routes wrap-params))
+
+(defn start-web [] (run-jetty app {:port 9876  :join? false}))
